4b6cf08ccf9c4b11d54352b0c3b8e4ddfbe1e727
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-08-30 23:35:37 +0530

    add footer items in code base

--- static/item.py
+++ static/item.py
@@ -2,4 +2,27 @@ navBarItems = {
    "Home" :{"url" : "#",},
    "About" :{"url" : "#",},
    "Contact" :{"url" : "#",},
+   "Team" :{"url" : "#",},
+}
+
+footerOrganizationItems = {
+   "Plantnet.org" :{"url" : "#",},
+   "Donate" :{"url" : "#",},
+   "Team" :{"url" : "#",},
+}
+footerResourcesItems = {
+    "FAQ" :{"url" : "#",},
+   "Open data" :{"url" : "#",},
+   "API" :{"url" : "#",},
+   "Stats" :{"url" : "#",},
+   "Translations" :{"url" : "#",},
+}
+footerLegalItems = {
+    "About" :{"url" : "#",},
+   "Terms of use" :{"url" : "#",},
+   "Legal terms" :{"url" : "#",},
+}
+footerContactUsItems = {
+    "Contact" :{"url" : "#",},
+   "System health" :{"url" : "#",},
 }
