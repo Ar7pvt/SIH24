46aae6a75795622a78d4e1cfff3f148bc46f0a20
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-08-30 22:04:14 +0530

    add app in code base

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, url_for
+from static import item
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/index1')
+def index1():
+    return render_template('index1.html')
+
+# item = ["Home","About","contact",]
+@app.route('/index2')
+def index2():
+    return render_template('index2.html',items = item.navBarItems)
+@app.route('/index3')
+def index3():
+    return render_template('index3.html',items = item.navBarItems)
 
 if __name__=="__main__":
     app.run(debug=True)
