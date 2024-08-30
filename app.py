024db0ea2f542c748b14c12842e00c1f8831808f
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-08-30 23:41:56 +0530

    add footer items in app

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def index2():
     return render_template('index2.html',items = item.navBarItems)
 @app.route('/index3')
 def index3():
-    return render_template('index3.html',items = item.navBarItems)
+    return render_template('index3.html',navbarItems = item.navBarItems,footerOrganizationItems = item.footerOrganizationItems,footerResourcesItems=item.footerResourcesItems,footerLegalItems=item.footerLegalItems,footerContactUsItems=item.footerContactUsItems)
 
 if __name__=="__main__":
     app.run(debug=True)
